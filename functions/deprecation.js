ccada9aaa27ee57cb57fcd1f5772f431f8d5f763
tyler-mairose-sp <tyler.mairose@sailpoint.com>
2021-09-21 12:06:41 -0400

    Add requried field check for parameters in paths and on schema objects

---
+++ functions/deprecation.js
@@ -0,0 +1,43 @@
+module.exports = (targetVal, _opts, paths) => {
+  results = [];
+
+  const rootPath = paths.target !== void 0 ? paths.target : paths.given;
+
+  if (targetVal.deprecated != undefined && targetVal.deprecated == true) {
+    if (targetVal.parameters != undefined && targetVal.parameters != null) {
+      deprecatedKeyFound = false;
+      sunsetKeyFound = false;
+      for (const [key, value] of Object.entries(targetVal.parameters)) {
+        if (JSON.stringify(value).indexOf('"in":"header","name":"Deprecation"') == 1) {
+            deprecatedKeyFound = true
+        } else if (JSON.stringify(value).indexOf('"in":"header","name":"Sunset"') == 1) {
+            sunsetKeyFound = true
+        }
+      }
+
+      if (!deprecatedKeyFound || !sunsetKeyFound) {
+        results.push({
+            message: `The path ${rootPath} must define deprecation and sunset dates in the header if api is marked as deprecated`,
+            path: [...rootPath, "deprecated"],
+          });
+      } else if (!sunsetKeyFound) {
+        results.push({
+            message: `The path ${rootPath} must define sunset date in the header if api is marked as deprecated`,
+            path: [...rootPath, "deprecated"],
+          });
+      } else if (!deprecatedKeyFound) {
+        results.push({
+            message: `The path ${rootPath} must define deprecation date in the header if api is marked as deprecated`,
+            path: [...rootPath, "deprecated"],
+          });
+      }
+    } else {
+      results.push({
+        message: `The path ${rootPath} must define deprecation and sunset dates in the header if api is marked as deprecated`,
+        path: [...rootPath, "deprecated"],
+      });
+    }
+  }
+  
+  return results;
+};
---
+++ functions/schema-object-field-check.js
@@ -0,0 +1,46 @@
+// Example Rule for function
+// Checks that a given field exists for root level schema objects, in the case of below we are checking that a required field is defined for 
+// schema-objects-must-have-required-parameters:
+// message: "${{error}}"
+// given: $
+// severity: error
+// then:
+//   function: schema-object-field-check
+//   functionOptions:
+//     field: required
+
+module.exports = (targetYaml, _opts, context, paths) => {
+  const { field } = _opts;
+  
+  results = [];
+
+  // All Of - If the root level yaml contains the key allOf
+  if (Object.keys(targetYaml)[0] == "allOf") {
+    targetYaml.allOf.forEach((element, index) => {
+      if (
+        element.type != undefined &&
+        element.type == "object" &&
+        !element.hasOwnProperty(field)
+      ) {
+        results.push({
+          message: `All schema objects must have a ${field} field defined`,
+          path: [`allOf`, parseInt(index), field],
+        });
+      }
+    });
+  }
+
+  // Type Object - If the root level yaml is of type object
+  if (
+    Object.keys(targetYaml).includes("type") &&
+    targetYaml.type == "object" &&
+    !targetYaml.hasOwnProperty(field)
+  ) {
+    results.push({
+      message: `All schema objects must have a ${field} field defined`,
+      path: [field],
+    });
+  }
+
+  return results;
+};
