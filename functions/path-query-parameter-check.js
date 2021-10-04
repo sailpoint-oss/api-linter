05ff664db6bd2fcee4402d3f8d8cbbeb3dd13f80
tyler-mairose-sp <tyler.mairose@sailpoint.com>
2021-10-04 14:20:25 -0400

    Fix for undefined parameters field and test for use case added

--- functions/path-query-parameter-check.js
+++ functions/path-query-parameter-check.js
@@ -1,6 +1,15 @@
 module.exports = (targetVal, _opts) => {
     const { rule, field } = _opts;
 
+    if (targetVal.parameters == undefined || targetVal.parameters == null) {
+        return [
+            {
+                message: `Rule ${rule}: All GET operations should have ${field} as a query parameter`
+            }
+        ]; 
+    }
+
+
     keyFound = false;
     for (const [key, value] of Object.entries(targetVal.parameters)) {
          if (JSON.stringify(value).indexOf(`"in":"query","name":"${field}"`) == 1) {
--- functions/tests/path-query-parameter-check-test.js
+++ functions/tests/path-query-parameter-check-test.js
@@ -73,6 +73,20 @@ let jsonParametersWithoutRequiredField = {
   ],
 };
 
+
+let jsonParametersEmpty = {
+  operationId: "listAccounts",
+  tags: ["Accounts"],
+  summary: "Accounts List This is a test",
+  description:
+    "This returns a list of accounts.  \nA token with ORG_ADMIN authority is required to call this API.",
+  security: [
+    {
+      bearerAuth: ["idn:account-list:read"],
+    },
+  ],
+};
+
 describe("Path Query Parameter Check", function () {
   it("Should not return any error message if the field exits in query parameters", function () {
     assert.deepEqual(
@@ -93,6 +107,21 @@ describe("Path Query Parameter Check", function () {
         },
       ],
       pathQueryParameterCheck(jsonParametersWithoutRequiredField, {
+        rule: ruleNumber,
+        field: requiredField,
+      })
+    );
+  });
+
+  it("Should return error message when the parameters object is missing or empty", function () {
+    assert.deepEqual(
+      [
+        {
+          message:
+            `Rule ${ruleNumber}: All GET operations should have ${requiredField} as a query parameter`,
+        },
+      ],
+      pathQueryParameterCheck(jsonParametersEmpty, {
         rule: ruleNumber,
         field: requiredField,
       })
