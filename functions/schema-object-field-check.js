afbe1fe2fa81a5bd12319ca826bf250288be4914
tyler-mairose-sp <tyler.mairose@sailpoint.com>
2021-12-06 16:14:41 -0500

    Update schema object field check to check if the field given exists but is not null or empty

--- functions/schema-object-field-check.js
+++ functions/schema-object-field-check.js
@@ -1,5 +1,5 @@
 // Example Rule for function
-// Checks that a given field exists for root level schema objects, in the case of below we are checking that a required field is defined for 
+// Checks that a given field exists for root level schema objects, in the case of below we are checking that a required field is defined for
 // schema-objects-must-have-required-parameters:
 // message: "${{error}}"
 // given: $
@@ -11,33 +11,40 @@
 
 module.exports = (targetYaml, _opts) => {
   const { rule, field } = _opts;
-  
+
+  console.dir(targetYaml);
   let results = [];
 
   // All Of - If the root level yaml contains the key allOf
   if (Object.keys(targetYaml)[0] == "allOf") {
     targetYaml.allOf.forEach((element, index) => {
       if (
-        element.type != undefined &&
-        element.type == "object" &&
-        !element.hasOwnProperty(field)
+        (element.type != undefined &&
+          element.type == "object" &&
+          element.hasOwnProperty(field) &&
+          (targetYaml[field] == null) ||
+    (targetYaml[field] != null && targetYaml[field].length == 0))
       ) {
         results.push({
-          message: `Rule ${rule}: All schema objects must have a ${field} field defined`,
+          message: `Rule ${rule}: If a ${field} key is defined for a schema object, it must not be null or empty`,
           path: [`allOf`, parseInt(index), field],
         });
       }
     });
   }
 
+  //console.log(targetYaml[field].length == 0)
+  //console.log((targetYaml[field] == null || targetYaml[field].length == 0))
   // Type Object - If the root level yaml is of type object
   if (
-    Object.keys(targetYaml).includes("type") &&
-    targetYaml.type == "object" &&
-    !targetYaml.hasOwnProperty(field)
+    (Object.keys(targetYaml).includes("type") &&
+      targetYaml.type == "object" &&
+      targetYaml.hasOwnProperty(field) &&
+      (targetYaml[field] == null) ||
+    (targetYaml[field] != null && targetYaml[field].length == 0))
   ) {
     results.push({
-      message: `Rule ${rule}: All schema objects must have a ${field} field defined`,
+      message: `Rule ${rule}: If a ${field} key is defined for a schema object, it must not be null or empty`,
       path: [field],
     });
   }
--- functions/tests/schema-object-field-test.js
+++ functions/tests/schema-object-field-test.js
@@ -60,7 +60,36 @@ let schemaObjectMissingRequiredField = {
   },
 };
 
-let schemaAllOfObjectMissingRequiredField = {
+let schemaObjectWithNullRequiredField = {
+  type: "object",
+  properties: {
+    oldApproverName: {
+      type: "string",
+      description: "Display name of approver that forwarded the approval.",
+      example: "frank.mir",
+    },
+    newApproverName: {
+      type: "string",
+      description:
+        "Display name of approver to whom the approval was forwarded.",
+      example: "al.volta",
+    },
+    comment: {
+      type: "string",
+      description: "Comment made by old approver when forwarding.",
+      example: "Fusce id orci vel consectetur amet ipsum quam.",
+    },
+    modified: {
+      type: "string",
+      format: "date-time",
+      description: "Time at which approval was forwarded.",
+      example: "2019-08-23T18:52:57.398Z",
+    },
+  },
+  required: null,
+};
+
+let schemaAllOfObjectWithMissingRequiredField = {
   allOf: [
     {
       type: "object",
@@ -119,6 +148,66 @@ let schemaAllOfObjectMissingRequiredField = {
   ],
 };
 
+let schemaAllOfObjectWithNullRequiredField = {
+  allOf: [
+    {
+      type: "object",
+      properties: {
+        oldApproverName: {
+          type: "string",
+          description: "Display name of approver that forwarded the approval.",
+          example: "frank.mir",
+        },
+        newApproverName: {
+          type: "string",
+          description:
+            "Display name of approver to whom the approval was forwarded.",
+          example: "al.volta",
+        },
+        comment: {
+          type: "string",
+          description: "Comment made by old approver when forwarding.",
+          example: "Fusce id orci vel consectetur amet ipsum quam.",
+        },
+        modified: {
+          type: "string",
+          format: "date-time",
+          description: "Time at which approval was forwarded.",
+          example: "2019-08-23T18:52:57.398Z",
+        },
+      },
+      required: [],
+    },
+    {
+      type: "object",
+      properties: {
+        oldApproverName: {
+          type: "string",
+          description: "Display name of approver that forwarded the approval.",
+          example: "frank.mir",
+        },
+        newApproverName: {
+          type: "string",
+          description:
+            "Display name of approver to whom the approval was forwarded.",
+          example: "al.volta",
+        },
+        comment: {
+          type: "string",
+          description: "Comment made by old approver when forwarding.",
+          example: "Fusce id orci vel consectetur amet ipsum quam.",
+        },
+        modified: {
+          type: "string",
+          format: "date-time",
+          description: "Time at which approval was forwarded.",
+          example: "2019-08-23T18:52:57.398Z",
+        },
+      },
+    },
+  ],
+};
+
 describe("Schema Object Field Check", function () {
   it("Should not return any errors when the given field is present", function () {
     assert.deepEqual(
@@ -130,14 +219,9 @@ describe("Schema Object Field Check", function () {
     );
   });
 
-  it("Should return errors when the given field is missing", function () {
+  it("Should not return any errors when the given field is missing", function () {
     assert.deepEqual(
-      [
-        {
-          message: `Rule ${ruleNumber}: All schema objects must have a ${field} field defined`,
-          path: ["required"],
-        },
-      ],
+      [],
       schemaObjectFieldCheck(schemaObjectMissingRequiredField, {
         rule: ruleNumber,
         field: field,
@@ -145,19 +229,42 @@ describe("Schema Object Field Check", function () {
     );
   });
 
-  it("Should return errors when the given field is missing in both allOf objects", function () {
+  it("Should return errors when the given field is present but is empty or null", function () {
     assert.deepEqual(
       [
         {
-          message: `Rule ${ruleNumber}: All schema objects must have a ${field} field defined`,
-          path: ["allOf", 0, "required"],
-        },
-        {
-            message: `Rule ${ruleNumber}: All schema objects must have a ${field} field defined`,
-            path: ["allOf", 1, "required"],
-          }
+          message: `Rule ${ruleNumber}: If a ${field} key is defined for a schema object, it must not be null or empty`,
+          path: ["required"],
+        }
       ],
-      schemaObjectFieldCheck(schemaAllOfObjectMissingRequiredField, {
+      schemaObjectFieldCheck(schemaObjectWithNullRequiredField, {
+        rule: ruleNumber,
+        field: field,
+      })
+    );
+  });
+
+
+  it("Should not return any errors when the given field is not present in the allOf format" , function () {
+    assert.deepEqual(
+      [],
+      schemaObjectFieldCheck(schemaAllOfObjectWithMissingRequiredField, {
+        rule: ruleNumber,
+        field: field,
+      })
+    );
+  });
+
+
+  it("Should return errors when the given field is present but null in the allOf format" , function () {
+    assert.deepEqual(
+      [
+        {
+          message: `Rule ${ruleNumber}: If a ${field} key is defined for a schema object, it must not be null or empty`,
+          path: ["allOf", 0 ,"required"],
+        }
+      ],
+      schemaObjectFieldCheck(schemaAllOfObjectWithNullRequiredField, {
         rule: ruleNumber,
         field: field,
       })
