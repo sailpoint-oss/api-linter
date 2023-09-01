1309a47fa739ee198c19e8f9ab551834bc3057ee
Philip Ellis <philip.ellis@sailpoint.com>
2023-09-01 08:18:11 -0500

    updated script to check for both securitySchemes

--- functions/path-oauth-scope-check.js
+++ functions/path-oauth-scope-check.js
@@ -12,10 +12,15 @@ module.exports = (targetVal, _opts) => {
 
   if (targetVal.security != undefined) {
     let oauth2exists = false;
-
+    let oauth2key = ""
     for (const [key, value] of Object.entries(targetVal.security)) {
-      if (value.oauth2 != undefined) {
+      if (value.UserContextAuth != undefined) {
         oauth2exists = true;
+        oauth2key = "UserContextAuth"
+      }
+      if (value.ApplicationOnlyAuth != undefined) {
+        oauth2exists = true;
+        oauth2key = "ApplicationOnlyAuth"
       }
     }
     if (!oauth2exists) {
@@ -26,14 +31,14 @@ module.exports = (targetVal, _opts) => {
       ];
     } else {
       for (const [key, value] of Object.entries(targetVal.security)) {
-        if (typeof(value.oauth2) !== "object") {
+        if (typeof(value[oauth2key]) !== "object") {
           return [
             {
               message: `Rule ${rule}: Operations must have security.oauth2 defined as an object with at least one scope defined to access the endpoint`,
             },
           ]
         } else {
-          for (const [oauthKey, oauthValue] of Object.entries(value.oauth2)) {
+          for (const [oauthKey, oauthValue] of Object.entries(value[oauth2key])) {
             let pattern = /^[a-z]+:[a-z-]+:[a-z]+$/;
             if(!pattern.test(oauthValue)) {
               return [
