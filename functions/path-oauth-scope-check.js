068b0ce0122f574dd5bb8040361d958d4579c2b2
Tyler Mairose <tyler.mairose@sailpoint.com>
2024-10-08 11:46:23 -0400

    Update oauth security scope check

--- functions/path-oauth-scope-check.js
+++ functions/path-oauth-scope-check.js
@@ -10,46 +10,67 @@
 module.exports = (targetVal, _opts) => {
   const { rule } = _opts;
 
+  let results = [];
   if (targetVal.security != undefined) {
-    let oauth2exists = false;
-    let oauth2key = ""
-    for (const [key, value] of Object.entries(targetVal.security)) {
-      if (value.userAuth != undefined) {
-        oauth2exists = true;
-        oauth2key = "userAuth"
-      }
-      if (value.applicationAuth != undefined) {
-        oauth2exists = true;
-        oauth2key = "applicationAuth"
-      }
-    }
-    if (!oauth2exists) {
-      return [
-        {
-          message: `Rule ${rule}: Operations must have security.userAuth and/or security.applicationAuth defined with at least one scope to access the endpoint`,
-        },
-      ];
+    let invalidKeys = [];
+    const validKeys = ["userAuth", "applicationAuth"];
+
+    targetVal.security.forEach((item) => {
+      const keys = Object.keys(item);
+      invalidKeys = keys.filter((key) => !validKeys.includes(key));
+    });
+
+    if (invalidKeys.length > 0) {
+      results.push({
+        message: `Rule ${rule}: Operations must have security.userAuth and/or security.applicationAuth defined with at least one scope to access the endpoint. ${invalidKeys} is not a valid key`,
+      });
+      return results;
     } else {
-      for (const [key, value] of Object.entries(targetVal.security)) {
-        if (typeof(value[oauth2key]) !== "object") {
-          return [
-            {
-              message: `Rule ${rule}: Operations must have security.userAuth and/or security.applicationAuth defined as an object with at least one scope defined to access the endpoint`,
-            },
-          ]
-        } else {
-          for (const [oauthKey, oauthValue] of Object.entries(value[oauth2key])) {
-            let pattern = /^[a-z]+:[a-z-]+:[a-z]+$/;
-            if(!pattern.test(oauthValue)) {
-              return [
-                {
-                  message: `Rule ${rule}: oauth2 scope "${oauthValue}" does not match the valid pattern for scopes. A valid example would be idn:access-profile:read`,
-                },
-              ]
-            }
+      const uniqueScopes = new Set();
+
+      // Iterate over each object in the array
+      targetVal.security.forEach((item) => {
+        if (item.userAuth !== undefined && !Array.isArray(item.userAuth)) {
+          results.push({
+            message: `Rule ${rule}: userAuth must be an array of one or more scopes`,
+          });
+        }
+
+        if (
+          item.applicationAuth !== undefined &&
+          !Array.isArray(item.applicationAuth)
+        ) {
+          results.push({
+            message: `Rule ${rule}: applicationAuth must be an array of one or more scopes`,
+          });
+        }
+
+        if (item.userAuth != undefined && Array.isArray(item.userAuth)) {
+          if (item.userAuth) {
+            item.userAuth.forEach((auth) => uniqueScopes.add(auth));
           }
         }
-      }
+
+        if (
+          item.applicationAuth != undefined &&
+          Array.isArray(item.applicationAuth)
+        ) {
+          if (item.applicationAuth) {
+            item.applicationAuth.forEach((auth) => uniqueScopes.add(auth));
+          }
+        }
+      });
+
+      uniqueScopes.forEach((scope) => {
+        let pattern = /^[a-z]+:[a-z-]+:[a-z]+$/;
+        if (!pattern.test(scope)) {
+          results.push({
+            message: `Rule ${rule}: oauth2 scope "${scope}" does not match the valid pattern for scopes. A valid example would be idn:access-profile:read`,
+          });
+        }
+      });
+
+      return results;
     }
   } else if (targetVal.security == null) {
     return [
