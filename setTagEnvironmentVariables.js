c3749fef317744ebcedcb4d2afd02182beb8f4fb
Tyler Mairose <tyler.mairose@sailpoint.com>
2024-11-05 15:08:46 -0500

    update script

--- setTagEnvironmentVariables.js
+++ setTagEnvironmentVariables.js
@@ -1,32 +1,32 @@
-// preloadTags.js
-const fs = require("fs");
-const path = require("path");
-const yaml = require("yaml");
+import fs from "fs";
+import path from "path";
+import yaml from "yaml";
+import { fileURLToPath } from "url";
 
-const startDir = "../api-specs/src/main/yaml";
+const startDir = new URL("./api-specs/src/main/yaml", import.meta.url);
 const fileName = process.argv[2];
-const fileContents = findAndReadFile(startDir, fileName);
+const fileContents = await findAndReadFile(startDir, fileName);
 
 if (fileContents !== null) {
   const parsedData = yaml.parse(fileContents);
-  // Output JSON string only, no other logs
   console.log(JSON.stringify(parsedData.tags.map((tag) => tag.name)));
 } else {
   console.error(`${fileName} file not found in any parent directories.`);
   process.exit(1);
 }
 
-function findAndReadFile(startDir, targetFile) {
+async function findAndReadFile(startDir, targetFile) {
   let currentDir = startDir;
 
-  while (currentDir !== path.parse(currentDir).root) {
-    const filePath = path.join(currentDir, targetFile);
+  while (currentDir.pathname !== "/") {
+    const filePath = path.join(fileURLToPath(currentDir), targetFile);
 
     if (fs.existsSync(filePath)) {
-      const fileContents = fs.readFileSync(filePath, "utf8");
+      const fileContents = await fs.promises.readFile(filePath, "utf8");
       return fileContents;
     }
-    currentDir = path.dirname(currentDir);
+
+    currentDir = new URL("..", currentDir);
   }
 
   return null;
