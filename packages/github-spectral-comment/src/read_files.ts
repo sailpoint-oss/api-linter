5cde768d812d0444c1868113d38e9252bb9b2adb
Luke Hagar <lukeslakemail@gmail.com>
2025-02-07 10:43:13 -0600

    testing action

--- packages/github-spectral-comment/src/read_files.ts
+++ packages/github-spectral-comment/src/read_files.ts
@@ -1,4 +1,3 @@
-import { toPosixPath } from "@actions/core";
 import * as fs from "node:fs";
 import path from "node:path";
 
@@ -9,6 +8,10 @@ export const readFilesToAnalyze = async (githubWorkspace: string, fileGlob: stri
   for (let i = 0, len = files.length; i < len; i++) {
     console.log(`FILE ${i} ${path.join(githubWorkspace, files[i])}`);
     let filePath = path.join(githubWorkspace, files[i]);
+    if (filePath.includes(".github")) {
+      console.log("Skipping GitHub Action files");
+      continue;
+    }
     try {
       if (fs.existsSync(filePath)) {
         console.log("File Exists, adding it to be linted");
