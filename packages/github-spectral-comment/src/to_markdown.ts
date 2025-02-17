3854ba0bc7016bcd6f2237e3da63535489a97eb7
Luke Hagar <lukeslakemail@gmail.com>
2025-02-17 13:31:56 -0600

    adding updated timestamp

--- packages/github-spectral-comment/src/to_markdown.ts
+++ packages/github-spectral-comment/src/to_markdown.ts
@@ -15,7 +15,8 @@ const toMarkdown = async (processedPbs: any, project: any) => {
   const nbHints = severitiesCount[3];
   const nbPbs = nbErrors + nbWarnings + nbInfos + nbHints;
 
-  let md = `Spectral Analysis
+  let md = `Spectral Analysis 
+Last updated: ${new Date().toISOString()}
 <details open>
 <summary>OpenAPI lint errors: ${nbPbs} problems (${nbErrors} errors, ${nbWarnings} warnings, ${nbInfos} infos, ${nbHints} hints)</summary>
 
