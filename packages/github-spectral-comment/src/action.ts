443d48ba5e6684fb030b65c5c2cd9222b3d18c44
Luke Hagar <lukeslakemail@gmail.com>
2025-02-19 16:47:22 -0600

    testing links

--- packages/github-spectral-comment/src/action.ts
+++ packages/github-spectral-comment/src/action.ts
@@ -86,7 +86,7 @@ export async function getGithubComment(
 
   core.debug(JSON.stringify(data, null, 2));
 
-  const comments = data.filter((comment) => comment?.user?.login === "github-actions[bot]" && comment?.body?.includes("OpenAPI linting report"))
+  const comments = data.filter((comment) => comment?.user?.login === "github-actions[bot]" && comment?.body?.includes("Last updated"))
   core.debug(`Found ${comments.length} matchingcomments`);
 
   let comment
