ca3f1a3d230e38ab9f991eae51a6a5d50ad876ed
Luke Hagar <lukeslakemail@gmail.com>
2025-02-19 16:26:53 -0600

    testing links

--- packages/github-spectral-comment/src/read_files.ts
+++ packages/github-spectral-comment/src/read_files.ts
@@ -1,30 +1,29 @@
-import * as fs from "node:fs";
+import { existsSync, readFileSync } from "node:fs";
 import path from "node:path";
 import { devLog } from "./utils.js";
+import core from "@actions/core";
 
 export const readFilesToAnalyze = async (githubWorkspace: string, fileGlob: string) => {
   const files = fileGlob.split(",");
   const fileContents = [];
 
   for (let i = 0, len = files.length; i < len; i++) {
-    devLog(githubWorkspace);
-    devLog(files[i]);
-    console.log(`FILE ${i} ${path.join(githubWorkspace, files[i])}`);
-    let filePath = path.join(githubWorkspace, files[i]);
+    const filePath = path.join(githubWorkspace, files[i]);
+    console.log(`Checking File #${i} ${filePath}`);
 
     if (filePath.includes(".github")) {
       console.log("Skipping GitHub Action files");
       continue;
     }
     try {
-      if (fs.existsSync(filePath)) {
+      if (existsSync(filePath)) {
         console.log("File Exists, adding it to be linted");
         fileContents.push({
           file: files[i],
-          content: fs.readFileSync(filePath, "utf-8"),
+          content: readFileSync(filePath, "utf-8"),
         });
       } else {
-        console.log("File does not exist, if the file was intentionally deleted during the PR ignore this comment");
+        core.warning("File does not exist, if the file was intentionally deleted during the PR ignore this comment");
       }
     } catch (err) {
       console.error(err);
