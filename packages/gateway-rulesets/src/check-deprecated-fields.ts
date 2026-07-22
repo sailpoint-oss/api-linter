3b18301124faeea50eb8ac9f5c36bfe26242de1a
Ben Godin <ben.godin@sailpoint.com>
2026-07-21 23:09:05 -0400

    APIMGMT-2136: refactoring check-deprecated-fields

--- packages/gateway-rulesets/src/check-deprecated-fields.ts
+++ packages/gateway-rulesets/src/check-deprecated-fields.ts
@@ -2,168 +2,90 @@
 
 import {createOptionalContextRulesetFunction} from "./createOptionalContextRulesetFunction.js";
 import {Route, VersionDetails} from "./types.js";
-import path from "path";
-import core from "@actions/core";
-import * as yaml from 'js-yaml';
-import {existsSync, readFileSync} from "node:fs";
+import {getMasterRouteIds} from "./utils.js";
 
 const newVersionIdPattern = /-v\d$|-internal$/
-const newPathPattern = /v\d$|internal$/
 const validAdditionalVersions: string[] = ["v3", "beta"]
 const versionYearPattern = /^v(\d{4})$/;
 const validVersionMapKeyPattern = /^beta$|^v3$|^v(202[4-9]|20[3-9][0-9]|2[1-9][0-9]{2})$/
 const deprecationYearPattern = /^\d{4}-\d{2}-\d{2}$/;
 const validApiStates = ["private", "limited-preview", "public-preview", "public"]
 
-interface MasterFileInput {
-    "file-glob"?: string
-}
-
-interface RouteYamlFile {
-    "sp-gateway": { routes: Route[] }
-}
-
-const getDevMasterInput = (isDev: boolean): MasterFileInput => {
-    const localInput = {
-        "file-glob": "../packages/test-files/master/sp-gateway-routes.yaml"
-    }
-
-    if (process.env.GITHUB_WORKSPACE) {
-        Object.fromEntries(
-            Object.keys(localInput).map((key) => [
-                key,
-                core.getInput(key, { required: !isDev }) ||
-                (isDev ? localInput[key as keyof MasterFileInput] : undefined),
-            ]),
-        );
-    }
-
-    return localInput
-};
-
-function readFilesToAnalyze (githubWorkspace: string, fileGlob: string): { file: string; content: string }[] {
-    const files = fileGlob.split(",");
-    const fileContents = [];
-
-    for (let i = 0, len = files.length; i < len; i++) {
-        const filePath = path.join(githubWorkspace, files[i]);
-        console.log(`Checking File in master branch #${i} ${filePath}`);
-
-        if (!filePath.includes("sp-gateway-routes")) {
-            console.log("Skipping unrelated files");
-            continue;
-        }
-        try {
-            if (existsSync(filePath)) {
-                console.log("File exists in master branch, adding it to the list to be compared to changed files");
-                fileContents.push({
-                    file: files[i],
-                    content: readFileSync(filePath, "utf-8"),
-                });
-            } else {
-                core.warning(
-                    "File does not exist in master branch, if the file was intentionally deleted during the PR ignore this comment",
-                );
-            }
-        } catch (err) {
-            console.error(err);
-        }
-    }
-
-    return fileContents;
-}
-
-function getMasterRouteIds(): Set<string> {
-    const workspace: string = process.env.GITHUB_WORKSPACE ? process.env.GITHUB_WORKSPACE + "/master/" : path.resolve()
-    const inputs: MasterFileInput = getDevMasterInput(process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test")
-
-    const masterFileContents = readFilesToAnalyze(workspace, inputs["file-glob"]!)
-
-    return new Set<string>(masterFileContents.map(file => {
-        const routeFile = yaml.load(file.content) as RouteYamlFile;
-        return routeFile["sp-gateway"].routes
-    }).flat().map(route => {
-        return route.id
-    }));
-}
-
 export default createOptionalContextRulesetFunction(
     {
         input: null,
         options: {},
-    },(routes: Route[], options: {}) => {
+    },(route: Route, options: {}) => {
         let results: {message: string}[] = [];
 
         const masterRouteIds = getMasterRouteIds();
-        console.log(masterRouteIds)
-        routes.forEach(route => {
-            if (!masterRouteIds.has(route.id) || (newVersionIdPattern.test(route.id) && newPathPattern.test(route.path))) {
-                if (route.versionStart && route.versionStart > 0) {
-                    results.push({
-                        message: `route ${route.id} uses a deprecated field: 'versionStart', and cannot be added to the route spec. Refer to **** for the new API versioning strategy.`
-                    });
-                }
 
-                if (route.additionalVersions) {
-                    results.push({
-                        message: `route ${route.id} uses a deprecated field: 'additionalVersions', and cannot be added to the route spec. Refer to **** for the new API versioning strategy.`
-                    });
-                }
-
-                if (route.versionEnd && route.versionEnd > 0) {
-                    results.push({
-                        message: `route ${route.id} uses a deprecated field: 'versionEnd', and cannot be added to the route spec. Refer to **** for the new API versioning strategy.`
-                    });
-                }
-
-                if (route.versionDetailsMap) {
-                    results.push({
-                        message: `route ${route.id} has a deprecated field: 'versionDetailsMap', and cannot be added to the route spec. Refer to **** for the new API versioning strategy.`
-                    });
-                }
-            } else {
-                if (!route.versionStart) {
-                    results.push({
-                        message: `route ${route.id} is missing a required field: 'versionStart'.`
-                    });
-                } else {
-                    results.push(...checkVersionStart(route.versionStart));
-                }
-
-                if (route.additionalVersions) {
-                    results.push(...checkAdditionalVersions(route.additionalVersions));
-                }
-
-                if (route.versionDetailsMap) {
-                    // javascript doesn't like maps, so we have to manually convert this to a Record
-                    const versionMap = route.versionDetailsMap as unknown as Record<string, VersionDetails>;
-                    Object.keys(versionMap).forEach((key) => {
-                        const details = versionMap[key]
-                        if (!validVersionMapKeyPattern.test(key)) {
-                            results.push({
-                                message: `route ${route.id} has a versionDetailsMap entry with an invalid key: ${key}. Must be a valid api version e.g. beta, v3 or v20XX.`
-                            })
-                        }
-
-                        if (details.apiState && !validApiStates.includes(details.apiState)) {
-                            results.push({
-                                message: `route ${route.id} has a versionDetailsMap entry ${key} with an invalid API state: ${details.apiState}. Must be private, limited-preview, public-preview, or public.`
-                            })
-                        }
-
-                        if (details.deprecation && !deprecationYearPattern.test(details.deprecation)) {
-                            results.push({
-                                message: `route ${route.id} has a versionDetailsMap entry ${key} with an improperly formatted deprecation date: ${details.deprecation}. Must be a valid date in yyyy-mm-dd format.`
-                            })
-                        }
-                    })
-
-                    results.push(...checkVersionDetailsVsVersionEnd(route))
-                }
-
-                results.push(...checkLatestVersionOverride(route))
+        if (!masterRouteIds.has(route.id) || newVersionIdPattern.test(route.id)) {
+            if (route.versionStart && route.versionStart > 0) {
+                results.push({
+                    message: `route ${route.id} uses a deprecated field: 'versionStart', and cannot be added to the route spec. Refer to https://sailpoint.atlassian.net/wiki/x/ZwHcNQE for the new API versioning strategy.`
+                });
             }
-        })
+
+            if (route.additionalVersions) {
+                results.push({
+                    message: `route ${route.id} uses a deprecated field: 'additionalVersions', and cannot be added to the route spec. Refer to https://sailpoint.atlassian.net/wiki/x/ZwHcNQE for the new API versioning strategy.`
+                });
+            }
+
+            if (route.versionEnd && route.versionEnd > 0) {
+                results.push({
+                    message: `route ${route.id} uses a deprecated field: 'versionEnd', and cannot be added to the route spec. Refer to https://sailpoint.atlassian.net/wiki/x/ZwHcNQE for the new API versioning strategy.`
+                });
+            }
+
+            if (route.versionDetailsMap) {
+                results.push({
+                    message: `route ${route.id} has a deprecated field: 'versionDetailsMap', and cannot be added to the route spec. Refer to https://sailpoint.atlassian.net/wiki/x/ZwHcNQE for the new API versioning strategy.`
+                });
+            }
+        } else {
+            if (!route.versionStart) {
+                results.push({
+                    message: `route ${route.id} is missing a required field: 'versionStart'.`
+                });
+            } else {
+                results.push(...checkVersionStart(route.versionStart));
+            }
+
+            if (route.additionalVersions) {
+                results.push(...checkAdditionalVersions(route.additionalVersions));
+            }
+
+            if (route.versionDetailsMap) {
+                // javascript doesn't like maps, so we have to manually convert this to a Record
+                const versionMap = route.versionDetailsMap as unknown as Record<string, VersionDetails>;
+                Object.keys(versionMap).forEach((key) => {
+                    const details = versionMap[key]
+                    if (!validVersionMapKeyPattern.test(key)) {
+                        results.push({
+                            message: `route ${route.id} has a versionDetailsMap entry with an invalid key: ${key}. Must be a valid api version e.g. beta, v3 or v20XX.`
+                        })
+                    }
+
+                    if (details.apiState && !validApiStates.includes(details.apiState)) {
+                        results.push({
+                            message: `route ${route.id} has a versionDetailsMap entry ${key} with an invalid API state: ${details.apiState}. Must be private, limited-preview, public-preview, or public.`
+                        })
+                    }
+
+                    if (details.deprecation && !deprecationYearPattern.test(details.deprecation)) {
+                        results.push({
+                            message: `route ${route.id} has a versionDetailsMap entry ${key} with an improperly formatted deprecation date: ${details.deprecation}. Must be a valid date in yyyy-mm-dd format.`
+                        })
+                    }
+                })
+
+                results.push(...checkVersionDetailsVsVersionEnd(route))
+            }
+
+            results.push(...checkLatestVersionOverride(route))
+        }
 
         return results;
     });
--- packages/gateway-rulesets/src/utils.ts
+++ packages/gateway-rulesets/src/utils.ts
@@ -1,4 +1,11 @@
 // Copyright (c) 2026. SailPoint Technologies, Inc. All rights reserved.
+import {createOptionalContextRulesetFunction} from "./createOptionalContextRulesetFunction.js";
+import {Route} from "./types.js";
+import path from "path";
+import core from "@actions/core";
+import * as yaml from 'js-yaml';
+import {existsSync, readFileSync} from "node:fs";
+
 enum VersionExemptPaths {
     OAUTH = "/oauth/"
 }
@@ -23,4 +30,82 @@ export function CheckInterfaceForField(objectName: string, objectToValidate: Obj
             message: `${objectName} has an invalid field: ${field}`
         });
     }
+}
+
+interface MasterFileInput {
+    "file-glob"?: string
+}
+
+interface RouteYamlFile {
+    "sp-gateway": { routes: Route[] }
+}
+
+let masterRouteIds = new Set<string>();
+
+const getDevMasterInput = (isDev: boolean): MasterFileInput => {
+    const localInput = {
+        "file-glob": "../packages/test-files/master/sp-gateway-routes.yaml"
+    }
+
+    if (process.env.GITHUB_WORKSPACE) {
+        Object.fromEntries(
+            Object.keys(localInput).map((key) => [
+                key,
+                core.getInput(key, { required: !isDev }) ||
+                (isDev ? localInput[key as keyof MasterFileInput] : undefined),
+            ]),
+        );
+    }
+
+    return localInput
+};
+
+function readFilesToAnalyze (githubWorkspace: string, fileGlob: string): { file: string; content: string }[] {
+    const files = fileGlob.split(",");
+    const fileContents = [];
+
+    for (let i = 0, len = files.length; i < len; i++) {
+        const filePath = path.join(githubWorkspace, files[i]);
+        console.log(`Checking File in master branch #${i} ${filePath}`);
+
+        if (!filePath.includes("sp-gateway-routes")) {
+            console.log("Skipping unrelated files");
+            continue;
+        }
+        try {
+            if (existsSync(filePath)) {
+                console.log("File exists in master branch, adding it to the list to be compared to changed files");
+                fileContents.push({
+                    file: files[i],
+                    content: readFileSync(filePath, "utf-8"),
+                });
+            } else {
+                core.warning(
+                    "File does not exist in master branch, if the file was intentionally deleted during the PR ignore this comment",
+                );
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return fileContents;
+}
+
+export function getMasterRouteIds(): Set<string> {
+    if (masterRouteIds.size === 0) {
+        const workspace: string = process.env.GITHUB_WORKSPACE ? process.env.GITHUB_WORKSPACE + "/master/" : path.resolve()
+        const inputs: MasterFileInput = getDevMasterInput(process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test")
+
+        const masterFileContents = readFilesToAnalyze(workspace, inputs["file-glob"]!)
+
+        masterRouteIds = new Set<string>(masterFileContents.map(file => {
+            const routeFile = yaml.load(file.content) as RouteYamlFile;
+            return routeFile["sp-gateway"].routes
+        }).flat().map(route => {
+            return route.id
+        }));
+    }
+
+    return masterRouteIds
 }
